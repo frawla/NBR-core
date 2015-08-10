f21efc05bf3beaf878842efa728155b372a53b39
rosemary <rosemary@kali.master.phd>
2015-08-11 02:15:24 +0300

    Replaced BeforeClass with Before annotation; and removed a couple of extra spaces

--- src/test/java/org/frawla/nbr/core/exporting/Test_SheetExporter.java
+++ src/test/java/org/frawla/nbr/core/exporting/Test_SheetExporter.java
@@ -6,25 +6,25 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.frawla.nbr.core.common.Util;
-import org.junit.BeforeClass;
+import org.junit.Before;
 import org.junit.Test;
 
 import de.schlichtherle.truezip.file.TFile;
 
 public class Test_SheetExporter
 {
-    @BeforeClass
-    public static void setUpBeforeClass() throws Exception
+    @Before
+    public void setup()
     {
-        //
+        //TODO please remove if this is not necessary
     }
 
     @Test
     public void Test_Export()
     {
         Map<String, String> resultMap = new HashMap<>();
-        String str = Util.readFileAsString(new TFile( Util.getTestResourcesPath("/expected_output.txt")));
-        resultMap.put(Util.getMainResourcesPath("/this file name.txt") , str);
+        String str = Util.readFileAsString(new TFile(Util.getTestResourcesPath("/expected_output.txt")));
+        resultMap.put(Util.getMainResourcesPath("/this file name.txt"), str);
         final SheetExporter sh = new SheetExporter();
         sh.export(resultMap);
         assertNotNull(sh.getWorkBook());
